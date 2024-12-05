4c5c33af3a917f99573c8be483dc316b92d4083b
mihai150 <mdaian150@yahoo.com>
2024-12-05 16:53:37 +0200

    day4

--- day4/day4_exe.py
+++ day4/day4_exe.py
@@ -4,9 +4,17 @@ import os
 # added the parent directory to python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from ceres_search import CeresSearch
 
 if __name__ == "__main__":
     
     # sample
+    ceres_search_sample = CeresSearch("sample_input.txt")
+    print("Count of linear XMAS in sample: " + str(ceres_search_sample.search_linear_xmas()))
+    print("Count of cross XMAS in sample: " + str(ceres_search_sample.search_cross_xmas())) 
+
     # real
-    
+    ceres_search_real = CeresSearch("input.txt")
+    print("Count of linear XMAS in real: " + str(ceres_search_real.search_linear_xmas()))
+    print("Count of cross XMAS in real: " + str(ceres_search_real.search_cross_xmas()))
+
